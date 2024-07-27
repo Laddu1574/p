95e3e446878f09506e45482600a1e51c38c46ad6
Thaniparthi Jagadeesh <126874364+Laddu1574@users.noreply.github.com>
2024-07-27 10:38:13 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,6 @@
 let type = new Typed('#changing-text',{
     strings : ['Front-end Developer','Web Developer','React js Expert','Java Expert','Word Press Expert'],
     typeSpeed: 50,
-    backSpedd: 50,
+    backSpeed: 50,
     loop: true,
 });
